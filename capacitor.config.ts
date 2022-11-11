73f90fbdcb6eb11ecdd4f3929ed89dacbeeb6c58
ManuEly19 <manuelauqui@outlook.com>
2022-11-11 16:47:21 -0500

    Proyecto de fotos

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'unaEnBlanco',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
